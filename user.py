aa8c28a57954f47b3f8b45d45b7575bc62e71a85
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-04 02:26:27 +0300

    add empty user list

--- user.py
+++ user.py
@@ -3,7 +3,8 @@ class User:
     Class that generates new instances of contacts
     """
     pass
-
+    user_list = [] # Empty user list
+    
     def __init__(self,username,password):
 
         '''
