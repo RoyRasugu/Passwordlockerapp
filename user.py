c0c2f25636195dbcdc20a88f48c8d046682f8351
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-04 01:51:36 +0300

    generate userclass

--- user.py
+++ user.py
@@ -1,3 +1,20 @@
+class User:
+    """
+    class that generates new instances of users
+    """
+    
+    pass
+def __init__(self,username,password):
+    '''
+    __init__ method that helps us define properties for our objects.
+
+    Args:
+        username: New user Username.
+        password: New user Password
+    '''
+
+    self.username = username
+    self.password = password
 
 
 
