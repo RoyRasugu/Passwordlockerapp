afefe0cb9aa18e375f743111fd363b7b7b526d1f
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-04 02:51:55 +0300

    add the first unittest condition

--- user_test.py
+++ user_test.py
@@ -9,3 +9,19 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.Testcase: Testcase class that helps in creating test cases
     '''
+    def setUp(self):
+        '''
+        Set up methodto run before each test cases.
+        '''
+        self.new_user = User("Ratchez","Pass1234")
+
+        def test_init(self):
+            '''
+            test_init test case to test if the object is initailized properly
+            '''
+
+            self.assertEqual(self.new_user.username,"Ratchez")
+            self.assertEqual(self.new_user.password,"Pass1234")
+
+if __name__ == '__main__':
+    unittest.main()
