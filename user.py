2f566d84815a76d4ed5a8df6bb482cc683f25e95
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-04 01:54:32 +0300

    define an init method with parameters

--- user.py
+++ user.py
@@ -5,6 +5,17 @@ class User:
     
     pass
 
+def __init__(self,username,password):
+    '''
+    __init__ method that helps us define properties for our objects.
+
+    Args:
+        username: New user Username.
+        password: New user Password
+    '''
+
+    self.username = username
+    self.password = password
 
 
 
