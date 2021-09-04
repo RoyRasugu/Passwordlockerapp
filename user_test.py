6377a44ea24d818deaf4a2d71c2f3198a3e4a0c7
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-04 04:04:00 +0300

    will add the teardown method

--- user_test.py
+++ user_test.py
@@ -34,8 +34,25 @@ class TestUser(unittest.TestCase):
  
     def test_save_multiple_user(self):
         '''
-        test_save_multiple_user to check if we can save multiple contact
+        test_save_multiple_user to check if we can save multiple user
         objects to our user_list
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Bree","nighty")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user_ to check if we can save multiple user
+        objects to our user_list 
         '''
 
         self.new_user.save_user()
