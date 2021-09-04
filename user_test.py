7e6a904ff2137a7a276f961a1b38e3b59c4eb029
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-04 03:12:00 +0300

    will create test to save user input

--- user_test.py
+++ user_test.py
@@ -11,22 +11,26 @@ class TestUser(unittest.TestCase):
     '''
     def setUp(self):
         '''
-        Set up methodto run before each test cases.
+        Set up method to run before each test cases.
         '''
         self.new_user = User("Ratchez","Pass1234")
 
-        def test_init(self):
-            '''
-            test_init test case to test if the object is initialized properly
-            '''
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
 
-            self.assertEqual(self.new_user.username,"Ratchez")
-            self.assertEqual(self.new_user.password,"Pass1234")
+        self.assertEqual(self.new_user.username,"Ratchez")
+        self.assertEqual(self.new_user.password,"Pass1234")
         
-        def test_save_user(self):
-            '''
-            test_
-            '''
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved in to
+        the user list
+        '''
+
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 
 if __name__ == '__main__':
     unittest.main()
