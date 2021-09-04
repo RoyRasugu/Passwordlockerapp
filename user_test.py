60e12509b2d528551869b86f944494a473b2fc6b
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-04 03:56:18 +0300

    will add multiple users

--- user_test.py
+++ user_test.py
@@ -31,6 +31,17 @@ class TestUser(unittest.TestCase):
 
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
+ 
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple contact
+        objects to our user_list
+        '''
 
+        self.new_user.save_user()
+        test_user = User("Bree","nighty")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+        
 if __name__ == '__main__':
     unittest.main()
