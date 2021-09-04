8a79513680cc1bb48bf72aaca415aa64405850fd
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-04 04:31:00 +0300

    implement delete user test method

--- user_test.py
+++ user_test.py
@@ -59,6 +59,18 @@ class TestUser(unittest.TestCase):
         test_user = User("Bree","nighty")
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)
-        
+    
+    def test_delete_user(self):
+        '''
+        test_delete_user to test if we can remove a user from our 
+        user list
+        '''
+        self.new_user.save_user()
+        test_user = User("Bree","nighty")
+        test_user.save_user()
+
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list),1)
+
 if __name__ == '__main__':
     unittest.main()
