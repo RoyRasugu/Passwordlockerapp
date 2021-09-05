64123c1b4008e81c94236d163d3610b3a44e47bd
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-05 12:06:12 +0300

    add the function to createa

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3.8
 from user import User
+
+def create_user(uname,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(uname,password)
+    return new_user
+
