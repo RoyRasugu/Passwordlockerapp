aad558ec2592ea89afd53c8901007fd334c5da21
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-06 02:15:52 +0300

    define properties of credentials

--- credentials.py
+++ credentials.py
@@ -1,7 +1,21 @@
 import random
+from run import display_users
 import string
 class Credentials:
     """
     Class that generates new instances of credentials
     """
     pass
+
+    def __init__(self,account,username,password):
+        '''
+        __init__method that helps us define properties for our objects.
+
+        Args:
+        account: New credential account.
+        username: New credential username.
+        password: New credential password.
+        '''
+        self.account = account
+        self.username = username
+        self.password = password
