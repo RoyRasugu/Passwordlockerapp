580b73a89f36214675d1fbaf3a672e122659715b
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-06 06:41:54 +0300

    test method to display all saved credentials

--- user_test.py
+++ user_test.py
@@ -174,6 +174,12 @@ class TestCredentials(unittest.TestCase):
         credential_exists = Credentials.credential_exist("Pinterest")
 
         self.assertTrue(credential_exists)
-
+    
+    def test_display_all_credentials(self):
+        '''
+        method that returns a list of all credentials saved
+        '''
+        self.assertEqual(Credentials.display_credentials(),Credentials.credentials_list)
+        
 if __name__ == '__main__':
     unittest.main()
